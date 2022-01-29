1af5872c50e0e672d8cf618da890fcc568a508a0
Krystian Zubrycki <xxrcd@eg.dk>
2022-01-29 15:31:13 +0100

    Configure PetController

--- src/main/java/guru/springframework/sfgdi/services/DogPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/DogPetService.java
@@ -1,13 +1,15 @@
 package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Service("pet")
 @Profile({"dog", "default"})
-public class DogPetService {
-    public String getPetType(){
+public class DogPetService implements PetService {
+    public String getPetType() {
         return "Dogs are the best!";
     }
 }
